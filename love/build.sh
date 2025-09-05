ced5d74261a0918d0ab2b52db64d7e9d51390469
am0iuyi <mzod73@gmail.com>
2025-09-05 20:55:21 +0400

    Update build.sh

--- love/build.sh
+++ love/build.sh
@@ -2,12 +2,14 @@
 set -o errexit
 
 echo ">>> Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
+
+echo ">>> Force installing whitenoise..."
+pip install whitenoise==6.6.0
 
 echo ">>> Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo ">>> Applying database migrations..."
 python manage.py migrate
-
-echo ">>> Build completed successfully!"
